a7b4d200e276f444900ede3c6cf5d0795c9d0837
KatarinaSundqvist <singsongkat@live.com>
2020-02-13 17:37:04 +0100

    Added Product and Customer Lists from xml

--- Common.Library/CommonBase.vb
+++ Common.Library/CommonBase.vb
@@ -1,4 +1,8 @@
-﻿Public Class CommonBase
+﻿Imports System.ComponentModel
+Imports System.Text
+
+Public Class CommonBase
+    Implements INotifyPropertyChanged
 
     Sub New()
         IsActive = True
@@ -9,6 +13,12 @@
     Public Property IsActive As Boolean
     Public Property ModifiedDate As DateTime
     Public Property CreatedBy As String
+
+    Public Event PropertyChanged As PropertyChangedEventHandler Implements INotifyPropertyChanged.PropertyChanged
+
+    Sub RaisePropertyChanged(ByVal propertyName As String)
+        RaiseEvent PropertyChanged(Me, New PropertyChangedEventArgs(propertyName))
+    End Sub
 
     Public Overrides Function ToString() As String
         Return GetClassData()
